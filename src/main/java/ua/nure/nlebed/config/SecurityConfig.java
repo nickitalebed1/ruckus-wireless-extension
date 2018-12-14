2600a004bc46df92332e48e148221f9957835280
nlebed <nikita.lebed@dataart.com>
2018-12-14 21:16:31 +0200

    add retrieve user json functionality delete spring security for some time

--- src/main/java/ua/nure/nlebed/config/SecurityConfig.java
+++ src/main/java/ua/nure/nlebed/config/SecurityConfig.java
@@ -2,41 +2,44 @@ package ua.nure.nlebed.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//@Configuration
+public class SecurityConfig
+//        extends WebSecurityConfigurerAdapter
+{
+
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/**").permitAll()
+//                .antMatchers("/rest/user")
+////                .antMatchers("/user/**").hasAnyRole("ADMIN")
+////                .antMatchers("/user/**").hasAnyRole("USER")
+////                .anyRequest().authenticated()
+////                .and()
+////                .formLogin()
+////                .loginPage("/login")
+////                .permitAll()
+////                .and()
+////                .logout()
+//                .permitAll();
+////                .and()
+////                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+//    }
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
-//                .antMatchers("/user/**").hasAnyRole("ADMIN")
-//                .antMatchers("/user/**").hasAnyRole("USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-//                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("password").roles("USER")
 //                .and()
-//                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
-    }
-
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER")
-                .and()
-                .withUser("admin").password("password").roles("ADMIN");
-    }
+//                .withUser("admin").password("password").roles("ADMIN");
+//    }
 }
--- src/main/java/ua/nure/nlebed/model/UserDetails.java
+++ src/main/java/ua/nure/nlebed/model/UserDetails.java
@@ -1,6 +1,5 @@
 package ua.nure.nlebed.model;
 
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
@@ -8,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -35,7 +33,6 @@ public class UserDetails {
     private boolean blocked;
 
     @ManyToOne
-//    @JoinColumn(name = "user_id")
     private User user;
 
 }
