a25f3bcfb931cc53e7c4341fbdd4030406fa516c
nlebed <nikita.lebed@dataart.com>
2018-12-19 18:08:26 +0200

    return saving admin

--- src/main/java/ua/nure/nlebed/SpringBootWebApplication.java
+++ src/main/java/ua/nure/nlebed/SpringBootWebApplication.java
@@ -9,7 +9,6 @@ import ua.nure.nlebed.model.Role;
 import ua.nure.nlebed.model.SupportedRoles;
 import ua.nure.nlebed.model.User;
 import ua.nure.nlebed.repository.UserRepository;
-import ua.nure.nlebed.service.UserService;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -29,15 +28,15 @@ public class SpringBootWebApplication implements CommandLineRunner {
 
     @Override
     public void run(String... strings) {
-//        User user = new User();
-//        Set<Role> roles = new HashSet<>();
-//        roles.add(new Role(SupportedRoles.ROLE_ADMIN));
-//        user.setEmail("admin@nure.ua");
-//        user.setLastName("admin");
-//        user.setName("admin");
-//        user.setPassword(passwordEncoder.encode("admin"));
-//        user.setRoles(roles);
-//        user.setPhotoUrl("https://www.timeshighereducation.com/sites/default/files/byline_photos/default-avatar.png");
-//        userRepository.saveAndFlush(user);
+        User user = new User();
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role(SupportedRoles.ROLE_ADMIN));
+        user.setEmail("admin@nure.ua");
+        user.setLastName("admin");
+        user.setName("admin");
+        user.setPassword(passwordEncoder.encode("admin"));
+        user.setRoles(roles);
+        user.setPhotoUrl("https://www.timeshighereducation.com/sites/default/files/byline_photos/default-avatar.png");
+        userRepository.saveAndFlush(user);
     }
 }
