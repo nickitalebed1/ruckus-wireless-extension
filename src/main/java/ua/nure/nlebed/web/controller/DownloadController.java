51697490e4accf003e314ac6ff30e225c89b04ee
nlebed <nikita.lebed@dataart.com>
2018-12-20 17:16:20 +0200

    add download android application functionality

---
+++ src/main/java/ua/nure/nlebed/web/controller/DownloadController.java
@@ -0,0 +1,32 @@
+package ua.nure.nlebed.web.controller;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.nure.nlebed.web.Navigation;
+import ua.nure.nlebed.web.Section;
+
+@Controller
+@Navigation(Section.DOWNLOADS)
+public class DownloadController {
+
+    @RequestMapping("/downloads")
+    public String downloads() {
+        return "downloads/index";
+    }
+
+    @RequestMapping(value="/downloads/android")
+    public ResponseEntity<InputStreamResource> downloadFile() {
+        InputStreamResource resource = new InputStreamResource(getClass().getClassLoader()
+                .getResourceAsStream("android/app.apk"));
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + "androidNureWifi.apk")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(resource);
+    }
+
+
+}
