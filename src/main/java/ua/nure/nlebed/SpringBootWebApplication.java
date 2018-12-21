0332b0db0feb4717ad124f3f990df5b0ddc12e8b
nlebed <nikita.lebed@dataart.com>
2018-12-22 01:41:54 +0200

    adding scheduler

--- src/main/java/ua/nure/nlebed/SpringBootWebApplication.java
+++ src/main/java/ua/nure/nlebed/SpringBootWebApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ua.nure.nlebed.model.Role;
 import ua.nure.nlebed.model.SupportedRoles;
@@ -14,6 +15,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootWebApplication implements CommandLineRunner {
 
     @Autowired
