b0bd2544d1a2f641bf18ed6e281b2c5eccbf95ab
nlebed <nikita.lebed@dataart.com>
2019-01-16 14:20:13 +0200

    change field to eager

--- src/main/java/ua/nure/nlebed/model/User.java
+++ src/main/java/ua/nure/nlebed/model/User.java
@@ -57,7 +57,7 @@ public class User {
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private Set<UserDetails> userDetails;
 
